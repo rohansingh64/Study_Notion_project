8cdc06f39252221f783eb87d5ffd09279c32c162
rohansingh64 <rohansinghrohansingh64@gmail.com>
2025-09-14 13:10:52 +0530

    responsive

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -4,76 +4,80 @@ import CourseCard from "./CourseCard";
 import HighlightText from "./HighlightText";
 
 const tabsName = [
-  "Free",
-  "New to coding",
-  "Most popular",
-  "Skills paths",
-  "Career paths",
+	"Free",
+	"New to coding",
+	"Most popular",
+	"Skills paths",
+	"Career paths",
 ];
 
 const ExploreMore = () => {
-  const [currentTab, setCurrentTab] = useState(tabsName[0]);
-  const [courses, setCourses] = useState(HomePageExplore[0].courses);
-  const [currentCard, setCurrentCard] = useState(
-    HomePageExplore[0].courses[0].heading
-  );
+	const [currentTab, setCurrentTab] = useState(tabsName[0]);
+	const [courses, setCourses] = useState(HomePageExplore[0].courses);
+	const [currentCard, setCurrentCard] = useState(
+		HomePageExplore[0].courses[0].heading
+	);
 
-  const setMyCards = (value) => {
-    setCurrentTab(value);
-    const result = HomePageExplore.filter((course) => course.tag === value);
-    setCourses(result[0].courses);
-    setCurrentCard(result[0].courses[0].heading);
-  };
+	const setMyCards = (value) => {
+		setCurrentTab(value);
+		const result = HomePageExplore.filter((course) => course.tag === value);
+		setCourses(result[0].courses);
+		setCurrentCard(result[0].courses[0].heading);
+	};
 
-  return (
-    <div>
-      {/* Explore more section */}
-      <div>
-        <div className="text-base sm:text-4xl font-semibold text-center my-10">
-          Unlock the
-          <HighlightText text={"Power of Code"} />
-          <p className="text-xs sm:text-lg font-semibold mt-1 text-center text-richblack-300">
-            Learn to Build Anything You Can Imagine
-          </p>
-        </div>
-      </div>
+	return (
+		<div>
+			{/* Explore more section */}
+			<div>
+				<div className="text-base sm:text-4xl font-semibold text-center my-10">
+					Unlock the
+					<HighlightText text={"Power of Code"} />
+					<p className="text-xs sm:text-lg font-semibold mt-1 text-center text-richblack-300">
+						Learn to Build Anything You Can Imagine
+					</p>
+				</div>
+			</div>
 
-      {/* Tabs Section */}
-      <div className="flex gap-3 sm:gap-5 -mt-5 mx-auto w-full sm:w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium overflow-x-auto scrollbar-hide drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
-        {tabsName.map((ele, index) => {
-          return (
-            <div
-              className={`text-sm sm:text-[16px] flex flex-row items-center whitespace-nowrap ${
-                currentTab === ele
-                  ? "bg-richblack-900 text-richblack-5 font-medium"
-                  : "text-richblack-200"
-              } px-5 sm:px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
-              key={index}
-              onClick={() => setMyCards(ele)}
-            >
-              {ele}
-            </div>
-          );
-        })}
-      </div>
+			{/* Tabs Section */}
+			<div
+				className="flex gap-3 sm:gap-5 -mt-5 mx-auto w-full sm:w-max 
+                bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium 
+                  overflow-x-auto scrollbar-hide drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]"
+			>
+				{tabsName.map((ele, index) => {
+					return (
+						<div
+							className={`text-sm sm:text-[16px] flex-shrink-0 flex flex-row items-center whitespace-nowrap ${
+								currentTab === ele
+									? "bg-richblack-900 text-richblack-5 font-medium"
+									: "text-richblack-200"
+							} px-5 sm:px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
+							key={index}
+							onClick={() => setMyCards(ele)}
+						>
+							{ele}
+						</div>
+					);
+				})}
+			</div>
 
-      <div className="hidden lg:block lg:h-[200px]"></div>
+			<div className="hidden lg:block lg:h-[200px]"></div>
 
-      {/* Cards Group */}
-      <div className="flex flex-wrap justify-center lg:justify-between gap-7 lg:gap-0 w-full text-black mb-7 lg:mb-0 px-3 lg:px-0 lg:absolute lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%]">
-        {courses.map((ele, index) => {
-          return (
-            <CourseCard
-              key={index}
-              cardData={ele}
-              currentCard={currentCard}
-              setCurrentCard={setCurrentCard}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+			{/* Cards Group */}
+			<div className="flex flex-wrap justify-center lg:justify-between gap-7 lg:gap-0 w-full text-black mb-7 lg:mb-0 px-3 lg:px-0 lg:absolute lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%]">
+				{courses.map((ele, index) => {
+					return (
+						<CourseCard
+							key={index}
+							cardData={ele}
+							currentCard={currentCard}
+							setCurrentCard={setCurrentCard}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default ExploreMore;
