f43a316ebfb1a0deb11295f0b304d1f9945bcd7b
rohansingh64 <rohansinghrohansingh64@gmail.com>
2025-09-14 13:06:56 +0530

    responsive home page

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -1,7 +1,3 @@
-
-// export default ExploreMore
-
-
 import React, { useState } from "react";
 import { HomePageExplore } from "../../../data/homepage-explore";
 import CourseCard from "./CourseCard";
@@ -33,25 +29,25 @@ const ExploreMore = () => {
     <div>
       {/* Explore more section */}
       <div>
-        <div className="sm:text-4xl text-base font-semibold text-center my-10">
+        <div className="text-base sm:text-4xl font-semibold text-center my-10">
           Unlock the
           <HighlightText text={"Power of Code"} />
-          <p className="text-center text-richblack-300 sm:text-lg text-xs font-semibold mt-1">
+          <p className="text-xs sm:text-lg font-semibold mt-1 text-center text-richblack-300">
             Learn to Build Anything You Can Imagine
           </p>
         </div>
       </div>
 
       {/* Tabs Section */}
-      <div className="hidden lg:flex gap-5 -mt-5 mx-auto w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
+      <div className="flex gap-3 sm:gap-5 -mt-5 mx-auto w-full sm:w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium overflow-x-auto scrollbar-hide drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
         {tabsName.map((ele, index) => {
           return (
             <div
-              className={` text-[16px] flex flex-row items-center gap-2 ${
+              className={`text-sm sm:text-[16px] flex flex-row items-center whitespace-nowrap ${
                 currentTab === ele
                   ? "bg-richblack-900 text-richblack-5 font-medium"
                   : "text-richblack-200"
-              } px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
+              } px-5 sm:px-7 py-[7px] rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5`}
               key={index}
               onClick={() => setMyCards(ele)}
             >
@@ -60,10 +56,11 @@ const ExploreMore = () => {
           );
         })}
       </div>
+
       <div className="hidden lg:block lg:h-[200px]"></div>
 
       {/* Cards Group */}
-      <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
+      <div className="flex flex-wrap justify-center lg:justify-between gap-7 lg:gap-0 w-full text-black mb-7 lg:mb-0 px-3 lg:px-0 lg:absolute lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%]">
         {courses.map((ele, index) => {
           return (
             <CourseCard
