832a09a598df2c37f24969c1a01fa051e567eddc
rohansingh64 <rohansinghrohansingh64@gmail.com>
2026-01-22 00:17:35 +0530

    for mail we use gmail

--- server/Utils/mailSender.js
+++ server/Utils/mailSender.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 const sendingMail = async (email,title,body,) => {
 	try {
 		const transporter = nodemailer.createTransport({
+			service:"Gmail",
 			host: process.env.MAIL_HOST,
 			port: process.env.MAIL_PORT,
-			secure: false, // true for port 465, false for 587
+			secure: true, // true for port 465, false for 587
 			auth: {
 				user: process.env.MAIL_USER,
 				pass: process.env.MAIL_PASS,
